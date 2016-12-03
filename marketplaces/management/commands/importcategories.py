d0e505c4d871f25351ead658f0be6ea7e94cb363
Paulo Pessoa <pvictorpessoa@hotmail.com>
2016-12-03 15:56:33 -0200

    add command importcategories, create_categories method and delete_old_categories

---
+++ marketplaces/management/commands/importcategories.py
@@ -0,0 +1,29 @@
+import sys
+from django.core.management.base import BaseCommand
+
+from marketplaces.models import Category, Channel
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('channel', type=str)
+        parser.add_argument('file_name', type=str)
+
+    def handle(self, *args, **options):
+        file_name = options.get('file_name', None)
+        channel = options.get('channel', None)
+        _channel, _ = Channel.objects.get_or_create(name=channel)
+        old_categories = _channel.categories.all()
+        try:
+            with open(file_name) as file:
+                lines = file.read().splitlines()
+                new_categories = Category.create_categories(_channel, lines)
+                Category.delete_old_categories(new_categories, old_categories)
+        except FileNotFoundError as error:
+            raise error
+
+        self.stdout.write(
+            '{0} : Total of Categories = {1}'.format(
+                _channel.name.title(),
+                _channel.categories.count())
+        )
--- marketplaces/models.py
+++ marketplaces/models.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from django.db import models
 
 # Create your models here.
@@ -18,10 +20,37 @@ class NameSlugMixin(models.Model):
 
 class Category(NameSlugMixin):
     parent = models.ForeignKey('self', null=True)
-    channel = models.ForeignKey('marketplaces.Channel')
+    channel = models.ForeignKey('marketplaces.Channel', related_name='categories')
 
     def __str__(self):
         return self.slug
+
+    @classmethod
+    def create_categories(cls, _channel, lines):
+        new_categories = []
+        for line in lines[1:len(lines) - 1]:
+            categories = line.split('/')
+            _parent = None
+            for category in categories:
+                _category, created = cls.objects.get_or_create(
+                    name=category,
+                    parent=_parent,
+                    channel=_channel
+                )
+                _parent = _category
+                if _category not in new_categories:
+                    new_categories.append(_category)
+            pprint(line)
+        return new_categories
+
+    @staticmethod
+    def delete_old_categories(new_categories, old_categories):
+        to_delete = [category for category in old_categories if category not in new_categories]
+        for _category in to_delete:
+            pprint('Category {0} deleted'.format(_category.name))
+            _category.delete()
+
+
 
 class Channel(NameSlugMixin):
     pass
