0f5d39dd4fc3b2df05ff168bda9dca4916929ab5
Paulo Pessoa <pvictorpessoa@hotmail.com>
2016-12-09 18:00:31 -0200

    add chanelistapitest and channeldetailapitest

--- marketplaces/tests/test_channel.py
+++ marketplaces/tests/test_channel.py
@@ -3,12 +3,17 @@ from model_mommy import mommy
 from rest_framework.test import APITestCase
 
 
-class ChannelTest(APITestCase):
+class ChannelBaseTest(APITestCase):
     def setUp(self):
-        _ = mommy.make(
+        self.channel = mommy.make(
             'Channel',
             name='walmart'
         )
+
+
+class ChannelListAPITest(ChannelBaseTest):
+    def setUp(self):
+        super().setUp()
         self.url_list = reverse('channel-list')
 
     def test_check_channel_on_list(self):
@@ -23,3 +28,28 @@ class ChannelTest(APITestCase):
         response = self.client.get(self.url_list)
         results = response.data.get('results')
         self.assertEqual(len(results), 50)
+
+    def test_invalid_post_method(self):
+        data = {
+            'name': 'Walmart'
+        }
+        response = self.client.post(self.url_list, data)
+        error_msg = response.data.get('detail')
+        self.assertEqual(405, response.status_code)
+        self.assertEqual('Method "POST" not allowed.', error_msg)
+
+
+class ChannelDetailAPITest(ChannelBaseTest):
+    def setUp(self):
+        super().setUp()
+        self.url_detail = reverse('channel-detail', kwargs={'slug': self.channel.slug})
+
+    def test_check_fields(self):
+        response = self.client.get(self.url_detail)
+        data = response.data
+        channel = self.channel
+        self.assertIn('slug', data)
+        self.assertIn('name', data)
+        self.assertIn('categories', data)
+        self.assertEqual(channel.slug, data.get('slug'))
+        self.assertEqual(channel.name, data.get('name'))
