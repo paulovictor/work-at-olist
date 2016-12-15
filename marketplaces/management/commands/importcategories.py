97124694721ed648c9de04c597d18ab34b0b12f6
Paulo Pessoa <pvictorpessoa@hotmail.com>
2016-12-15 18:03:45 -0200

    move logic of validate extension file; add validate params method of file

--- marketplaces/management/commands/importcategories.py
+++ marketplaces/management/commands/importcategories.py
@@ -14,21 +14,32 @@ class Command(BaseCommand):
         channel = options.get('channel', None)
         _channel, _ = Channel.objects.get_or_create(name=channel)
         old_categories = _channel.categories.all().select_related('parent')
-        file_extension = file_name.split('.')[-1]
-        valid_extensions = 'csv'
-        if file_extension == valid_extensions:
+
+        if self.validate_file_extension(file_name):
             try:
                 with open(file_name) as file:
                     lines = file.read().splitlines()
+                    self.validate_file_params(lines)
                     new_categories = Category.create_categories(_channel, lines)
                     Category.delete_old_categories(new_categories, old_categories)
             except FileNotFoundError as error:
                 raise error
-        else:
-            raise FormatError('We only accept csv format.')
 
         self.stdout.write(
             '{0} : Total of Categories = {1}'.format(
                 _channel.name.title(),
                 _channel.categories.count())
         )
+
+    @staticmethod
+    def validate_file_extension(file_name):
+        file_extension = file_name.split('.')[-1]
+        valid_extensions = 'csv'
+        if file_extension == valid_extensions:
+            return True
+        raise FormatError('We only accept csv format.')
+
+    @staticmethod
+    def validate_file_params(lines):
+        if 'Category' not in lines or ':' not in lines:
+            raise ValueError('Missing Category or :')
