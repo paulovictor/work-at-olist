2e1532c478b27adee3c6a7516f0ddf863bb8aad7
Paulo Pessoa <pvictorpessoa@hotmail.com>
2016-12-15 14:09:37 -0200

    add formaterror exception when receive an invalid extension

--- marketplaces/management/commands/importcategories.py
+++ marketplaces/management/commands/importcategories.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 
+from marketplaces.format_exception import FormatError
 from marketplaces.models import Category, Channel
 
 
@@ -13,13 +14,18 @@ class Command(BaseCommand):
         channel = options.get('channel', None)
         _channel, _ = Channel.objects.get_or_create(name=channel)
         old_categories = _channel.categories.all().select_related('parent')
-        try:
-            with open(file_name) as file:
-                lines = file.read().splitlines()
-                new_categories = Category.create_categories(_channel, lines)
-                Category.delete_old_categories(new_categories, old_categories)
-        except FileNotFoundError as error:
-            raise error
+        file_extension = file_name.split('.')[-1]
+        valid_extensions = 'csv'
+        if file_extension == valid_extensions:
+            try:
+                with open(file_name) as file:
+                    lines = file.read().splitlines()
+                    new_categories = Category.create_categories(_channel, lines)
+                    Category.delete_old_categories(new_categories, old_categories)
+            except FileNotFoundError as error:
+                raise error
+        else:
+            raise FormatError('We only accept csv format.')
 
         self.stdout.write(
             '{0} : Total of Categories = {1}'.format(
